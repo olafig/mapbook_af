163a203f061436d28ab50e5c5a7488f2e5fd8686
patrycjakre <patrycja.krecisz@student.wat.edu.pl>
2024-11-25 13:42:09 +0100

    chore: add new users

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ users:list=[
     {'name':'Szymon z wąsem','posts':5,'city':'Toruń'},
     {'name':'Szymon','posts':7,'city':'Białogard'},
     {'name':'Patryk','posts':9,'city':'Łódź'},
+    {'name':'Patrycja','posts':11,'city':'Mińsk'},
+    {'name':'Ola','posts':13,'city':'Radom'},
+    {'name':'Julia','posts':15,'city':'Zamość'},
+    {'name':'Karolina','posts':17,'city':'Mława'},
+    {'name':'Amelia','posts':19,'city':'Toruń'},
 
 ]
 #TODO please update user list
