ee77b83825bda8d1163a6af39f1a065a4368b876
olafig <aleksandra.figarska@student.wat.edu.pl>
2024-12-02 12:58:13 +0100

    feat: add create new user

--- mapbook/crud.py
+++ mapbook/crud.py
@@ -7,3 +7,11 @@ def hello(user:str)->None:
 def read_users(users:list)->None:
         for user in users[1:]:
             print(f'twój znajomy  {user['name']}, z miasta {user['city']} opublikował {user['posts']} postów')
+
+
+def add_user(userlist:list)->None:
+    new_name:str=input('proszę podać imię nowego znajomego')
+    new_post:int=int(input('podaj liczbę postów'))
+    new_city:str=input('podaj nazwę miasta')
+    new_user: dict = {'name': new_name, 'posts': new_post, 'city': new_city}
+    userlist.append(new_user)
